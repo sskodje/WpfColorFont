dd3ddc163078c33152b2515a9fa2c7aaf6a112a1
Amen Jlili <amen@bluebyte.biz>
2022-05-06 00:08:09 -0700

    small changes Added nuspec file Compiled to local package cdn

--- TestProject.Framework/MainWindow.xaml.cs
+++ TestProject.Framework/MainWindow.xaml.cs
@@ -28,7 +28,6 @@ namespace TestProject.Framework
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             ColorFontDialog dialog = new ColorFontDialog(true,true,true);
-           
             dialog.Owner = this;
             dialog.Font = FontInfo.GetControlFont(this.TextBlockSample);
             //dialog.FontSizes = new int[] { 10, 12, 14, 16, 18 };
